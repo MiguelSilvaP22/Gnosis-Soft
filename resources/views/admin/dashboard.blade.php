26d8f3addf5df74fccc7973ee9759c63c10cf686
cristopherbaeza <cristopher.baeza@inacapmail.cl>
2018-04-03 02:05:17 -0300

    Cambio de Portada

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,9 +1,13 @@
 @extends('admin.layout')
 
 @section ('content')
+<body> 
+	<div style="text-align: center;">
+			<h1>TWICE</h1>	
+			<img src="/adminlte/img/sana_sexy.jpg" class="img-circle" style="width: 300px;height: 450px;"/>
+			<img src="https://www.allkpop.com/upload/2017/10/af/17125018/sana.jpg" class="img-circle" /> 
+			<img src="/adminlte/img/sana_sexy.jpg" class="img-circle" style="width: 300px;height: 450px;"/>
 
-	<h1>TWICE</h1>
-	
-	<body> <img src="https://www.allkpop.com/upload/2017/10/af/17125018/sana.jpg" /> </body>
-	
+	</div>
+</body>
 @stop

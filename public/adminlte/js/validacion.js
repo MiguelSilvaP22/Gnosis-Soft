2b81a9c22c5e88b03fd002cbe002f88e328e40a1
José Pérez <jose.perez89@inacapmail.cl>
2018-05-24 23:05:49 -0400

    Validación
    a

--- public/adminlte/js/validacion.js
+++ public/adminlte/js/validacion.js
@@ -3,8 +3,14 @@ function marcarErrorGeneral(id,idErr){
 	document.getElementById(id).style.background='#FFEEEE'; document.getElementById(id).style.borderColor='#FF3333'; 
 	document.getElementById(idErr).innerHTML='Campo Obligatorio';
 	document.getElementById(idErr).style.color = '#FF3333';
-
 }
+
+function marcarErrorCharGer(id,idErr){
+	document.getElementById(id).style.background='#FFEEEE'; document.getElementById(id).style.borderColor='#FF3333'; 
+	document.getElementById(idErr).innerHTML='No se permiten caracteres especiales.';
+	document.getElementById(idErr).style.color = '#FF3333';
+}
+
 function desmarcarErrorGeneral(id,idErr){
 	document.getElementById(id).style.background="";
 	document.getElementById(id).style.borderColor="";
@@ -13,8 +19,28 @@ function desmarcarErrorGeneral(id,idErr){
 
 function validarGerencia (){
 	var verificar = true;
-	if($.trim( $("#nombre_gerencia").val()) == "" ){verificar = false; marcarErrorGeneral('nombre_gerencia','errNombreGerencia');}
+	var error = 0;
+
+	if($.trim( $("#nombre_gerencia").val()) == "" )
+	{error = 1;}
 	else{desmarcarErrorGeneral('nombre_gerencia','errNombreGerencia');}
+	
+	if(validarChr($("#nombre_gerencia").val()))
+	{error = 2;}
+	else{desmarcarErrorGeneral('nombre_gerencia','errNombreGerencia');}
+	
+	if(error == 1)
+	{verificar = false; marcarErrorGeneral('nombre_gerencia','errNombreGerencia');}
+
+	if(error == 2)
+	{verificar = false; marcarErrorCharGer('nombre_gerencia','errNombreGerencia');}
+
 	return verificar;
     	
 };
+
+function validarChr(TCode){
+	if( /^[a-zA-Z0-9- ]*$/.test( TCode ) ) 
+	{return false;}
+    return true;     
+}

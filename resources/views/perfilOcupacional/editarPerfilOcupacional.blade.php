851d40a960249ffc0855c394b9d525ff42086dc1
José Pérez <jose.perez89@inacapmail.cl>
2018-05-28 18:50:19 -0400

    Termino de validación organigrama

--- resources/views/perfilOcupacional/editarPerfilOcupacional.blade.php
+++ resources/views/perfilOcupacional/editarPerfilOcupacional.blade.php
@@ -10,19 +10,21 @@
 				</div>
 
 				<div class="box-body">
-				{!! Form::model($perfilOcu, ['method' => 'PATCH', 'action' => ['PerfilOcupacionalController@update',$perfilOcu->id_perfilocu],'id'=>'formPerfilOcupacional']) !!}
+					{!! Form::model($perfilOcu, ['method' => 'PATCH', 'action' => ['PerfilOcupacionalController@update',$perfilOcu->id_perfilocu],'id'=>'formPerfilOcupacional']) !!}
 				<div class='form-group'>
 					{!! Form::label('nombre_perfilocu', 'Nombre:') !!}
-					{!! Form::text('nombre_perfilocu', null, ['class' => 'form-control','maxlength'=>'100']) !!}
+					{!! Form::text('nombre_perfilocu', null, ['class' => 'form-control','id' => 'nombre_perfilOcu','maxlength'=>'100']) !!}
+					{!! Form::label('', '',['id' => 'errNombrePerfilOcu']) !!}
 				</div>
 				<div class='form-group'>
 					{!! Form::label('nombre_comp', 'Competencias:') !!}
 					{!! Form::select('id_comp[]', $competencias,null ,['class' => 'select2', 'multiple','id'=>'id_comp', 'style'=>'width:100%']) !!}
+					{!! Form::label('', '',['id' => 'errSelectComp']) !!}
 				</div>	
 				<div class='form-group'>
 					{!! Form::submit("Editar Perfil Ocuácional", ['class' => 'form-control btn btn-success ']) !!}
 				</div>
-				{!! Form::close() !!}
+					{!! Form::close() !!}
 				<div class='form-group'>
 					<div id="btnVolver" class="form-control btn btn-success " > Volver </div>
 				</div>
@@ -45,12 +47,14 @@ $('#formPerfilOcupacional').submit(function (e) {
 	e.preventDefault();  // prevent the form from 'submitting'
 	var url = e.target.action  // get the target
 	var formData = $(this).serialize() // get form data
-	$.post(url, formData, function (response) { // send; response.data will be what is returned
-		$('#modal').modal('hide');
-		refrescarPerfil({{$perfilOcu->id_area}});
-		})
-})
-
+	if(validarPerfilOcupacional())
+	{
+		$.post(url, formData, function (response) { // send; response.data will be what is returned
+			$('#modal').modal('hide');
+			refrescarPerfil({{$perfilOcu->id_area}});
+			});
+	}		
+});
 $(document).on('click', '#btnVolver', function () {
 	$('#modal').modal('hide');
 });

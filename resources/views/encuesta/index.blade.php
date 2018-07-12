18ab22d74a3416f72182e1d978889f925eafe06a
José Pérez <jose.perez89@inacapmail.cl>
2018-07-12 01:09:21 -0400

    a

--- resources/views/encuesta/index.blade.php
+++ resources/views/encuesta/index.blade.php
@@ -1,7 +1,11 @@
 @extends('admin.layout')
 
 @section ('content')
-
+<h1>Mantenedor Encuestas</h1>
+<ol class="breadcrumb">
+	<li><a href="/admin"><i class="fa fa-dashboard"></i>Inicio</a></li>
+	<li class="active">Encuestas</li>
+</ol> 
 <body> 
   <div class="row">
 		<div class="col-xs-12">
@@ -89,7 +93,30 @@
 $(document).ready(function() {
 
     $('#tablaEncuesta').DataTable({
-			
+		"language": {
+			"sProcessing":     "Procesando...",
+			"sLengthMenu":     "Mostrar _MENU_ registros",
+			"sZeroRecords":    "No se encontraron resultados",
+			"sEmptyTable":     "Ningún dato disponible en esta tabla",
+			"sInfo":           "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+			"sInfoEmpty":      "Mostrando registros del 0 al 0 de un total de 0 registros",
+			"sInfoFiltered":   "(filtrado de un total de _MAX_ registros)",
+			"sInfoPostFix":    "",
+			"sSearch":         "Buscar:",
+			"sUrl":            "",
+			"sInfoThousands":  ",",
+			"sLoadingRecords": "Cargando...",
+			"oPaginate": {
+				"sFirst":    "Primero",
+				"sLast":     "Último",
+				"sNext":     "Siguiente",
+				"sPrevious": "Anterior"
+			},
+			"oAria": {
+				"sSortAscending":  ": Activar para ordenar la columna de manera ascendente",
+				"sSortDescending": ": Activar para ordenar la columna de manera descendente"
+			}
+		}
 		});
 	$("#modal").on("hidden.bs.modal", function(){
    		$("#datos").html("");
